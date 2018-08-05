695cedeeb3da387501c52ae4c013eddf192614a4
zhanglu6 <zhanglu6@doumi.com>
2018-08-05 18:14:51 +0800

    LoopQuequ and arrayqueue 效率

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,22 +1,55 @@
+import java.util.Random;
+
 public class Main {
 
+    private static double testQueue(Queue<Integer> q, int opCount) {
+
+        long start = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+
+            int ran = random.nextInt(Integer.MAX_VALUE);
+            q.enQueue(ran);
+            System.out.println("enqueue: " + ran);
+        }
+
+        for (int i = 0; i < opCount; i++) {
+            System.out.println("dequeue: " + q.deQueue());
+        }
+        long end = System.nanoTime();
+
+        return (end - start) / 1000000000;
+    }
+
     public static void main(String[] args) {
-	    ArrayQueue<Integer> queue = new ArrayQueue<>();
+//	    ArrayQueue<Integer> queue = new ArrayQueue<>();
+//
+//	    for (int i = 0; i < 10; i++) {
+//	        queue.enQueue(i);
+//        }
+//
+//        System.out.println(queue);
+//
+//
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(String.format("size: %d", queue.getSize()));
+//            System.out.println(String.format("front: %d", queue.getFront()));
+//            System.out.println(String.format("dequeue: %d", queue.deQueue()));
+//            System.out.println(queue);
+//
+//        }
 
-	    for (int i = 0; i < 10; i++) {
-	        queue.enQueue(i);
-        }
+        int opCount = 100000;
 
-        System.out.println(queue);
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
 
+        double arrayCost = testQueue(arrayQueue, opCount);
+        System.out.println("arrayQueue cost: " + arrayCost + "s");
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println(String.format("size: %d", queue.getSize()));
-            System.out.println(String.format("front: %d", queue.getFront()));
-            System.out.println(String.format("dequeue: %d", queue.deQueue()));
-            System.out.println(queue);
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
 
-        }
+        double loopCost = testQueue(loopQueue, opCount);
+        System.out.println("loopQueue cost: " + loopCost + "s");
 
     }
 }
